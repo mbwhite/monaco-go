c996ffd5930c6f6f402bcc1550f4d9cf2de8cdb8
mbana <m.bana@synthace.com>
2016-10-25 21:47:16 +0100

    * remove 'use strict'. * ignore .vscode-test.

--- src/goMode.ts
+++ src/goMode.ts
@@ -2,11 +2,9 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-'use strict';
-
-import {WorkerManager} from './workerManager';
-import {GoWorker} from './goWorker';
-import {LanguageServiceDefaultsImpl} from './monaco.contribution';
+import { WorkerManager } from './workerManager';
+import { GoWorker } from './goWorker';
+import { LanguageServiceDefaultsImpl } from './monaco.contribution';
 import * as languageFeatures from './languageFeatures';
 
 import Promise = monaco.Promise;
