346ebfb8d950b6d139d0db80d960c830c3291d10
Mohamed Bana <m@bana.io>
2016-12-09 22:28:51 +0000

    add Languages.match method - see comment for the vscode src. link.

--- src/fillers/vscode.ts
+++ src/fillers/vscode.ts
@@ -1,6 +1,9 @@
 import Emitter = monaco.Emitter;
 import IEvent = monaco.IEvent;
 import IModel = monaco.editor.IModel;
+import {
+	DocumentSelector, DocumentFilter
+} from 'vscode-languageclient';
 
 declare type TextDocument = IModel;
 
@@ -107,20 +110,102 @@ class Workspace {
 	}
 }
 
-export function CompletionItem() { };
-export function CodeLens() { };
-export function Disposable() { };
-export const languages = {
-	createDiagnosticCollection() {
-		return {};
+class MonacoWindow {
+	constructor() {
 	}
-};
-export const workspace = new Workspace();
-export const window = {
+
 	createOutputChannel(name) {
 		return new OutputChannel(name);
-	},
-	showErrorMessage() { },
-	showInformationMessage() { },
-	showWarningMessage() { },
-};
+	}
+
+	showErrorMessage(message: string, ...items: string[]) {
+		console.error(message, ...items);
+		return Promise.resolve();
+	}
+
+	showInformationMessage(message: string, ...items: string[]) {
+		console.info(message, ...items);
+		return Promise.resolve();
+	}
+
+	showWarningMessage(message: string, ...items: string[]) {
+		console.warn(message, ...items);
+		return Promise.resolve();
+	}
+}
+
+
+
+class MonacoLanguages {
+	constructor() {
+
+	}
+
+	// see this file:
+	// https://github.com/Microsoft/vscode/blob/c67ef57cda90b5f28499646f7cc94e8dcc5b0586/src/vs/editor/common/modes/languageSelector.ts
+
+	/**
+	 * Compute the match between a document [selector](#DocumentSelector) and a document. Values
+	 * greater than zero mean the selector matches the document. The more individual matches a selector
+	 * and a document have, the higher the score is. These are the abstract rules given a `selector`:
+	 *
+	 * ```
+	 * (1) When selector is an array, return the maximum individual result.
+	 * (2) When selector is a string match that against the [languageId](#TextDocument.languageId).
+	 * 	(2.1) When both are equal score is `10`,
+	 * 	(2.2) When the selector is `*` score is `5`,
+	 * 	(2.3) Else score is `0`.
+	 * (3) When selector is a [filter](#DocumentFilter) return the maximum individual score given that each score is `>0`.
+	 *	(3.1) When [language](#DocumentFilter.language) is set apply rules from #2, when `0` the total score is `0`.
+	 *	(3.2) When [scheme](#DocumentFilter.scheme) is set and equals the [uri](#TextDocument.uri)-scheme score with `10`, else the total score is `0`.
+	 *	(3.3) When [pattern](#DocumentFilter.pattern) is set
+	 * 		(3.3.1) pattern equals the [uri](#TextDocument.uri)-fsPath score with `10`,
+	 *		(3.3.1) if the pattern matches as glob-pattern score with `5`,
+	 *		(3.3.1) the total score is `0`
+	 * ```
+	 *
+	 * @param selector A document selector.
+	 * @param document A text document.
+	 * @return A number `>0` when the selector matches and `0` when the selector does not match.
+	 */
+	public match(selector: DocumentSelector, document: TextDocument): number {
+		if (Array.isArray(selector)) {
+			// for each
+			let values = (<DocumentSelector[]>selector).map(item => this._match(item, document));
+			return Math.max(...values);
+		} else if (typeof selector === 'string') {
+			return this._match(selector, document);
+		} else {
+			return 0;
+		}
+	}
+
+	private _match(selector: DocumentSelector, document: TextDocument) {
+		if (typeof selector === 'string') {
+			let modeId = document.getModeId();
+			return modeId === selector ? 1 : 0;
+		} else {
+			return 0;
+		}
+	}
+
+	/**
+	 * Create a diagnostics collection.
+	 *
+	 * @param name The [name](#DiagnosticCollection.name) of the collection.
+	 * @return A new diagnostic collection.
+	 */
+	public createDiagnosticCollection() {
+		return {};
+	}
+}
+
+export function CompletionItem() { };
+
+export function CodeLens() { };
+
+export function Disposable() { };
+
+export const workspace = new Workspace();
+export const window = new MonacoWindow();
+export const languages = new MonacoLanguages();
