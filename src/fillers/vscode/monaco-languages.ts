1b7aca0be4b7821430f07e0c952e59784dfcba93
Mohamed Bana <m@bana.io>
2016-12-11 20:03:02 +0000

    add the rest of the features: * HoverProvider. * DefinitionProvider. * ReferenceProvider. * DocumentSymbolProvider. * WorkspaceSymbolProvider = noop.
    next:
    create the monaco model so that it gets a different uri instead of
    the in memory version.
    
    —
    
    --> request #2: textDocument/hover: {"textDocument":{"uri":"inmemory://model/1"},"position":{}}
    langserver-go: readMessages - m.request c.h.Handle - m.request: &{Method:textDocument/hover Params:0xc42019f460 ID:2 Meta:<nil> Notif:false}
    langserver-go: Handle - req: &{Method:textDocument/hover Params:0xc42019f460 ID:2 Meta:<nil> Notif:false}
    langserver-go: Handle InitTracer - req: &{Method:textDocument/hover Params:0xc42019f460 ID:2 Meta:<nil> Notif:false}, err: <nil>
    langserver-go: Handle err - ctx: 0xc420202ef0 conn: 0xc420202f00, req: 0xc42010b860, err: unexpected panic: bad uri "inmemory://model/1" (path "inmemory://model/1" MUST have leading slash; it can't be relative)
    panic serving textDocument/hover: bad uri "inmemory://model/1" (path "inmemory://model/1" MUST have leading slash; it can't be relative)
    goroutine 27 [running]:
    github.com/sourcegraph/go-langserver/langserver.(*LangHandler).Handle.func1(0xc4200fd810, 0xc420202ef0, 0xc420202f00, 0xc42010b860)
    	/Users/mbana/go/src/github.com/sourcegraph/go-langserver/langserver/handler.go:102 +0x20e

--- src/fillers/vscode/monaco-languages.ts
+++ src/fillers/vscode/monaco-languages.ts
@@ -1,6 +1,13 @@
 import Emitter = monaco.Emitter;
 import IEvent = monaco.IEvent;
 import Uri = monaco.Uri;
+import Disposable = monaco.IDisposable;
+
+import HoverProvider = monaco.languages.HoverProvider;
+import DefinitionProvider = monaco.languages.DefinitionProvider;
+import ReferenceProvider = monaco.languages.ReferenceProvider;
+import DocumentSymbolProvider = monaco.languages.DocumentSymbolProvider;
+// import WorkspaceSymbolProvider = monaco.languages.WorkspaceSymbolProvider
 
 import {
 	DocumentSelector, DocumentFilter,
@@ -20,33 +27,59 @@ export class MonacoLanguages {
 	constructor() {
 	}
 
+	private _getLanguageId(selector: DocumentSelector): string {
+		const LANGAUGE_ID_DEFAULT: string = 'go';
+
+		let languageId: string;
+		if (Array.isArray(selector) && selector.length > 1) {
+			languageId = <string>selector[0];
+		} else if (typeof selector === 'string') {
+			languageId = selector;
+		} else {
+			languageId = LANGAUGE_ID_DEFAULT;
+		}
+		return languageId;
+	}
+
+	// ”capabilities;": {;
+	//         "textDocumentSync";: 1,
+	//         "hoverProvider";: true,
+	//         "definitionProvider";: true,
+	//         "referencesProvider";: true,
+	//         "documentSymbolProvider";: true,
+	//         "workspaceSymbolProvider";: true;
+	//     }
+
+	registerHoverProvider(selector: DocumentSelector, provider: HoverProvider): Disposable {
+		let languageId = this._getLanguageId(selector);
+		return monaco.languages.registerHoverProvider(languageId, provider);
+	}
+
+	registerDefinitionProvider(selector: DocumentSelector, provider: DefinitionProvider): Disposable {
+		let languageId = this._getLanguageId(selector);
+		return monaco.languages.registerDefinitionProvider(languageId, provider);
+	}
+
+	registerReferenceProvider(selector: DocumentSelector, provider: ReferenceProvider): Disposable {
+		let languageId = this._getLanguageId(selector);
+		return monaco.languages.registerReferenceProvider(languageId, provider);
+	}
+
+	registerDocumentSymbolProvider(selector: DocumentSelector, provider: DocumentSymbolProvider): Disposable {
+		let languageId = this._getLanguageId(selector);
+		return monaco.languages.registerDocumentSymbolProvider(languageId, provider);
+	}
+
+	registerWorkspaceSymbolProvider(provider: any): Disposable {
+		return {
+			dispose() {
+			}
+		};
+	}
+
 	// see this file:
 	// https://github.com/Microsoft/vscode/blob/c67ef57cda90b5f28499646f7cc94e8dcc5b0586/src/vs/editor/common/modes/languageSelector.ts
 
-	/**
-	 * Compute the match between a document [selector](#DocumentSelector) and a document. Values
-	 * greater than zero mean the selector matches the document. The more individual matches a selector
-	 * and a document have, the higher the score is. These are the abstract rules given a `selector`:
-	 *
-	 * ```
-	 * (1) When selector is an array, return the maximum individual result.
-	 * (2) When selector is a string match that against the [languageId](#TextDocument.languageId).
-	 * 	(2.1) When both are equal score is `10`,
-	 * 	(2.2) When the selector is `*` score is `5`,
-	 * 	(2.3) Else score is `0`.
-	 * (3) When selector is a [filter](#DocumentFilter) return the maximum individual score given that each score is `>0`.
-	 *	(3.1) When [language](#DocumentFilter.language) is set apply rules from #2, when `0` the total score is `0`.
-	 *	(3.2) When [scheme](#DocumentFilter.scheme) is set and equals the [uri](#TextDocument.uri)-scheme score with `10`, else the total score is `0`.
-	 *	(3.3) When [pattern](#DocumentFilter.pattern) is set
-	 * 		(3.3.1) pattern equals the [uri](#TextDocument.uri)-fsPath score with `10`,
-	 *		(3.3.1) if the pattern matches as glob-pattern score with `5`,
-	 *		(3.3.1) the total score is `0`
-	 * ```
-	 *
-	 * @param selector A document selector.
-	 * @param document A text document.
-	 * @return A number `>0` when the selector matches and `0` when the selector does not match.
-	 */
 	match(selector: DocumentSelector, document: TextDocument): number {
 		if (Array.isArray(selector)) {
 			// for each
@@ -59,7 +92,7 @@ export class MonacoLanguages {
 		}
 	}
 
-	private _match(selector: DocumentSelector, document: TextDocument) {
+	private _match(selector: DocumentSelector, document: TextDocument): number {
 		if (typeof selector === 'string') {
 			let modeId = document.model.getModeId();
 			return modeId === selector ? 1 : 0;
@@ -68,12 +101,6 @@ export class MonacoLanguages {
 		}
 	}
 
-	/**
-	 * Create a diagnostics collection.
-	 *
-	 * @param name The [name](#DiagnosticCollection.name) of the collection.
-	 * @return A new diagnostic collection.
-	 */
 	createDiagnosticCollection() {
 		return {};
 	}
