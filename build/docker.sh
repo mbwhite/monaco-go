926de0930644c9ad877d2e45db8743ac73887ab5
Mohamed Bana <m@bana.io>
2017-01-09 22:18:35 +0000

    Dockerfile - speed up the build time - separate parts into different images:
    * `base-langserver`, `base-go-langserver`, `go-langserver`, and `monaco-go`.
    * move `docker-entrypoint.sh` to build/docker.

--- build/docker.sh
+++ build/docker.sh
@@ -1,23 +1,81 @@
 #!/bin/sh
 
-# hack to invalidate Docker cache
-touch README.md
+BASEDIR=$(dirname "$0")
 
-REPO_NAME=$(basename `git rev-parse --show-toplevel`)
-GIT_REV=$(git rev-parse --short HEAD)
-
-IMAGE_NAME_REV="$REPO_NAME:$GIT_REV"
-IMAGE_NAME_LATEST="$REPO_NAME:latest"
-
-docker build \
-  -t $IMAGE_NAME_REV \
-  -t $IMAGE_NAME_LATEST \
-  -f ./build/docker/Dockerfile \
-  .
 
 echo "--------"
-echo "run commands:"
-echo "docker run -p 8080:8080 -p 4389:4389 -it $IMAGE_NAME_REV"
-echo "docker run -p 8080:8080 -p 4389:4389 -it $IMAGE_NAME_LATEST"
+IMAGE_NAME_PREFIX="mohamedbana"
+REPO_NAME=$(basename `git rev-parse --show-toplevel`)
+GIT_REV=$(git rev-parse --short HEAD)
+echo " prefix:" $IMAGE_NAME_PREFIX
+echo "   repo:" $REPO_NAME
+echo "    rev:" $GIT_REV
+echo "--------"
 
-# docker run -p 8080:8080 -it $IMAGE_NAME_LATEST
+echo "--------"
+IMAGE_BASE_FOLDER="base-langserver"
+IMAGE_BASE_NAME="$IMAGE_NAME_PREFIX/$IMAGE_BASE_FOLDER"
+IMAGE_BASE_LATEST="$IMAGE_BASE_NAME:latest"
+# only build the latest one for now
+# docker build \
+#   -t $IMAGE_BASE_REV \
+#   -t $IMAGE_BASE_LATEST \
+#   -f ./build/docker/${IMAGE_BASE_FOLDER}/Dockerfile \
+#   .
+echo "> image:" "$IMAGE_BASE_NAME @ $IMAGE_BASE_LATEST"
+
+docker build \
+  -t $IMAGE_BASE_LATEST \
+  -f ./build/docker/${IMAGE_BASE_FOLDER}/Dockerfile \
+  .
+echo "< image:" "$IMAGE_BASE_NAME @ $IMAGE_BASE_LATEST"
+echo "--------"
+
+
+echo "--------"
+IMAGE_BASE_GO_LANGSERVER_FOLDER="base-go-langserver"
+IMAGE_BASE_GO_LANGSERVER_NAME="$IMAGE_NAME_PREFIX/$IMAGE_BASE_GO_LANGSERVER_FOLDER"
+IMAGE_BASE_GO_LANGSERVER_LATEST="$IMAGE_BASE_GO_LANGSERVER_NAME:latest"
+echo "> image:" "$IMAGE_BASE_GO_LANGSERVER_NAME @ $IMAGE_BASE_GO_LANGSERVER_LATEST"
+
+docker build \
+  -t $IMAGE_BASE_GO_LANGSERVER_LATEST \
+  -f ./build/docker/${IMAGE_BASE_GO_LANGSERVER_FOLDER}/Dockerfile \
+  .
+echo "< image:" "$IMAGE_BASE_GO_LANGSERVER_NAME @ $IMAGE_BASE_GO_LANGSERVER_LATEST"
+echo "--------"
+
+
+echo "--------"
+IMAGE_GO_LANGSERVER_FOLDER="go-langserver"
+IMAGE_GO_LANGSERVER_NAME="$IMAGE_NAME_PREFIX/$IMAGE_GO_LANGSERVER_FOLDER"
+# IMAGE_GO_LANGSERVER_REV="$IMAGE_GO_LANGSERVER_NAME:$GIT_REV"
+IMAGE_GO_LANGSERVER_LATEST="$IMAGE_GO_LANGSERVER_NAME:latest"
+echo "> image:" "$IMAGE_GO_LANGSERVER_NAME @ $IMAGE_GO_LANGSERVER_LATEST"
+
+docker build \
+  -t $IMAGE_GO_LANGSERVER_LATEST \
+  -f ./build/docker/${IMAGE_GO_LANGSERVER_FOLDER}/Dockerfile \
+  .
+echo "< image:" "$IMAGE_GO_LANGSERVER_NAME @ $IMAGE_GO_LANGSERVER_LATEST"
+echo "--------"
+
+
+echo "--------"
+IMAGE_MONACO_GO_FOLDER="monaco-go"
+IMAGE_MONACO_GO_NAME="$IMAGE_NAME_PREFIX/$IMAGE_MONACO_GO_FOLDER"
+IMAGE_MONACO_GO_LATEST="$IMAGE_MONACO_GO_NAME:latest"
+echo "> image:" "$IMAGE_MONACO_GO_NAME @ $IMAGE_MONACO_GO_LATEST"
+
+docker build \
+  -t $IMAGE_MONACO_GO_LATEST \
+  -f ./build/docker/${IMAGE_MONACO_GO_FOLDER}/Dockerfile \
+  .
+echo "< image:" "$IMAGE_MONACO_GO_NAME @ $IMAGE_MONACO_GO_LATEST"
+echo "--------"
+
+
+echo "--------"
+echo "run cmds:"
+echo "docker run -p 8080:8080 -p 4389:4389 -it $IMAGE_MONACO_GO_LATEST"
+echo "--------"
