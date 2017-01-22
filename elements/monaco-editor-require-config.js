b37bdeb9014e0eebc5c90da17e9758e46eb72204
Mohamed Bana <m@bana.io>
2017-01-22 14:22:35 +0000

    reduce files stored on azure vm: * load monaco-editor from https://microsoft.github.io/monaco-editor/. * need to find the cdn links for polymer next - as most of the fetches are for polymer elements.

--- elements/monaco-editor-require-config.js
+++ elements/monaco-editor-require-config.js
@@ -1,16 +1,35 @@
+// store current paths
+let baseUrl = location.href;
+// let baseUrl = `${currentPath}/monaco-go/`;
+
+
+// <script src="https://cdn.jsdelivr.net/lodash/4.17.2/lodash.min.js" integrity="sha256-IyWBFJYclFY8Pn32bwWdSHmV4B9M5mby5bhPHEmeY8w=" crossorigin="anonymous"></script>
+// <script src="https://cdn.jsdelivr.net/lodash/4.17.2/lodash.js" integrity="sha256-jCRPoAgIIooCTnLmaSyKMPrFgFh6/T0e8c3i+KkZZ6U=" crossorigin="anonymous"></script>
+
 // Loading basic-languages to get the css language definition
 var require = {
-	baseUrl: '/monaco-go/',
+	baseUrl,
 	paths: {
-		'lodash': 'node_modules/lodash/lodash',
+		'lodash': 'https://cdn.jsdelivr.net/lodash/4.17.2/lodash.min.js',
 		'vscode': 'out/fillers/vscode',
 
 		'vscode-languageserver-types': 'node_modules/vscode-languageserver-types/lib/main',
 		'vscode-jsonrpc': 'node_modules/vscode-languageclient/node_modules/vscode-jsonrpc/lib/main',
 		'vscode-languageclient': 'node_modules/vscode-languageclient/lib/main',
 
-		'vs/basic-languages': 'node_modules/monaco-languages/release',
+		'vs/basic-languages': 'https://microsoft.github.io/monaco-editor/node_modules/monaco-editor/min/vs/basic-languages',
 		'vs/language/go': 'release/dev',
-		'vs': 'node_modules/monaco-editor-core/dev/vs'
+		// 'vs': 'node_modules/monaco-editor-core/dev/vs',
+		'vs': 'https://microsoft.github.io/monaco-editor/node_modules/monaco-editor/min/vs',
+	}
+};
+
+// Before loading vs/editor/editor.main, define a global MonacoEnvironment that overwrites
+// the default worker url location (used when creating WebWorkers). The problem here is that
+// HTML5 does not allow cross-domain web workers, so we need to proxy the instantion of
+// a web worker through a same-domain script
+window.MonacoEnvironment = {
+	getWorkerUrl: function(workerId, label) {
+		return 'elements/monaco-editor-worker-loader-proxy.js';
 	}
 };
---
+++ elements/monaco-editor-worker-loader-proxy.js
@@ -0,0 +1,4 @@
+// self.MonacoEnvironment = {
+// 	baseUrl: 'https://microsoft.github.io/monaco-editor/node_modules/monaco-editor/min/'
+// };
+importScripts('https://microsoft.github.io/monaco-editor/node_modules/monaco-editor/min/vs/base/worker/workerMain.js');
